b2dff9b20ba2b2ba4a2c84a4716889d15f934bd4
Zergei64 <zsoftz@gmail.com>
2017-05-10 11:39:22 -0600

    Add files via upload

---
+++ test/test_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+RSpec.describe "something" do
+  it "does something" do
+    #expect(5).to eq 5
+    expect(SimpleNumber.new(2).add(2)).to eq 4
+    expect(SimpleNumber.new(2).multiply(3)).to eq 6
+  end
+end
+
+RSpec.describe "failme" do
+  it "does a fail" do
+    #expect(5).to eq 5
+    SimpleNumber.new(0).nothing()
+  end
+end
